342e0e73589a2d22f2bd8e6710a9fbed8018e3c2
timze <timze@timzedeMacBook.local>
2020-02-19 14:20:04 +0800

    update

--- csv2rds.R
+++ csv2rds.R
@@ -1,16 +1,22 @@
-c <- read.csv('lastest/city.csv',stringsAsFactors = F)
+df <- read.csv('lastest/city.csv',stringsAsFactors = F)
 
-colnames(c) <-  c('time','province','city','city_confirmed','city_cured','city_dead','city_suspected',
-           'confirmed','suspected','cured','dead','comment')
-c$time = as.Date(c$time,format='%Y-%m-%d') 
+colnames(df) <-  c('time','province','city','city_confirmed','city_cured','city_dead','city_suspected',
+                  'confirmed','suspected','cured','dead','comment')
+df$time = as.Date(df$time,format='%Y-%m-%d') 
+city <- df[,1:7]
+colnames(city) <- c('time','province','city','confirmed','cured','dead','suspected')
+province <- df[,c(1,2,8:11)]
+colnames(province) <- c('time','province','confirmed','cured','dead','suspected')
+
 Dataframe_2_S3 <- function(data){
   S3 <- list(
-    data = data,
+    data = city,
+    province = province,
     time = max(data$time) 
   )
   class(S3) <- 'nCov2019History'
   return(S3)
 }
 
-b <- Dataframe_2_S3(c)
+b <- Dataframe_2_S3(df)
 saveRDS(b,'historical_data.rds')
