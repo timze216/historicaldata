dd6cb16206c824590ff8210e218a4b402ab3ce47
timze <timze@timzedeMacBook.lan>
2020-02-20 10:15:53 +0800

    update

--- csv2rds.R
+++ csv2rds.R
@@ -4,9 +4,9 @@ colnames(df) <-  c('time','province','city','city_confirmed','city_cured','city_
                   'confirmed','suspected','cured','dead','comment')
 df$time = as.Date(df$time,format='%Y-%m-%d') 
 city <- df[,1:7]
-colnames(city) <- c('time','province','city','confirmed','cured','dead','suspected')
+colnames(city) <- c('time','province','city','cum_confirm','cum_heal','cum_dead','suspected')
 province <- df[,c(1,2,8:11)]
-colnames(province) <- c('time','province','confirmed','cured','dead','suspected')
+colnames(province) <- c('time','province','cum_confirm','cum_heal','cum_dead','suspected')
 
 Dataframe_2_S3 <- function(data){
   S3 <- list(
